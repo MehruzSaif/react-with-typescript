b94d1d7109cdc6405188ae93a903bf18ae89c169
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-06-01 05:23:13 +0600

    string or boolean with turnary ops

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,22 @@ let isSmart: boolean = true;
 let students: string[] = ['Joe', 'Baiden', 'Trump', 'Obama'];
 let fees: number[] = [12, 21, 14, 44, 55]; 
 
+interface Person { // interface usually started with cpatal letter
+  name: string,
+  job?: string,
+  employed?: string | boolean,
+  age: number,
+  location: string | number
+}
+
+const person: Person = {
+  name: 'Bill Clinton',
+  // job: 'Jobless',
+  employed: true,
+  location: 55,
+  age: 85,
+}
+
 function App() {
   return (
     <div className="App">
