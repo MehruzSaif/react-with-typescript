940ed36e8e22071914040aa85c94b4243bb4055b
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-06-02 04:50:49 +0600

    types with useState and event handler

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 
 const Counter = () => {
 
+    interface User {
+        name: string,
+        job: string
+    }
+
     const [counter, setCounter] = useState<number>(0);
+    const [user, setUser] = useState<User | null>(null);
 
     const handleIncrease = (): void => {
         setCounter(counter + 1);
@@ -11,11 +17,20 @@ const Counter = () => {
         setCounter(counter - 1);
     }
 
+    const handleUserSubmit = (e) => {
+        e.preventDefault();
+        const userData = {
+            name: 'Adam',
+            job: 'Dev'
+        }
+        setUser(userData)
+    }
+
     return (
         <div>
             <h1>This is counter</h1>
             <h2>{counter}</h2>
-            <button onClick={handleIncrease}>+</button> <span></span>
+            <button onClick={handleIncrease}>+</button> <span> Quantity </span>
             <button onClick={handleDecrease}>-</button>
         </div>
     );
